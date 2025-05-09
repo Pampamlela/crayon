d4fdbaa9f1da4ae2a8722198e2b4107bc99cfeba
Pampamlela <pampamlela@gmail.com>
2025-05-09 09:38:51 +0200

    first commit

---
+++ crayon.js
@@ -0,0 +1,26 @@
+/*Crée une fonction drawMyPen(){:js} qui retourne l'état de ton crayon en ASCII Art 
+en fonction du temps passé en cours. 
+Tu trouveras un exemple de crayons ci-dessous. 
+Attention, petite subtilité : la gomme a deux étages en début d'année scolaire, 
+mais se réduit à un seul étage à partir du mois de mars.Exemple :
+
+drawMyPen('septembre');
+// retourne un crayon à 10 étages avec une gomme à 2 étages
+ /\
+/__\
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+|__|
+|  |
+|__|
+*/
+
+drawMyPen(){}
